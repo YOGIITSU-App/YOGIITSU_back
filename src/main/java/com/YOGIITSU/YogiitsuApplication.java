93de7b9a974d30386130bbc7757225744eab49f7
parksomii <somi020605@gmail.com>
2025-01-10 20:21:06 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/YOGIITSU/YogiitsuApplication.java
@@ -0,0 +1,11 @@
+package com.YOGIITSU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogiitsuApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(YogiitsuApplication.class, args);
+	}
+}
---
+++ src/test/java/com/YOGIITSU/YogiitsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YOGIITSU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogiitsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
