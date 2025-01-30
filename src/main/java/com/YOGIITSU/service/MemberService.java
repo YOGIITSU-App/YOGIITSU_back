2e8d71de6432e440ae1fad5ee7749e364418b408
parksomii <somi020605@gmail.com>
2025-01-31 00:28:28 +0900

    feat: MemberService에 이메일로 아이디 찾기 기능 추가

--- src/main/java/com/YOGIITSU/service/MemberService.java
+++ src/main/java/com/YOGIITSU/service/MemberService.java
@@ -44,4 +44,16 @@ public class MemberService {
 			throw new RuntimeException("아이디 또는 비밀번호가 잘못되었습니다", e);
 		}
 	}
+	/**
+	 * 이메일로 아이디 찾기
+	 *
+	 * @param email 사용자의 이메일
+	 * @return 사용자 아이디
+	 */
+	public String findIdByEmail(String email) {
+		// 이메일로 사용자 조회
+		return memberRepository.findByEmail(email)
+			.map(Member::getMemberId)
+			.orElse(null); // 가입된 이메일이 없으면 null 반환
+	}
 }
