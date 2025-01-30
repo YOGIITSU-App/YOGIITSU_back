5591b2ab0d23ba927af6131dfeb07f5f78639152
joyes0ng <128571665+joyes0ng@users.noreply.github.com>
2025-01-31 02:31:10 +0900

    feat: 문의 응답을 위한 InquiryResponseDto 클래스 추가

---
+++ src/main/java/com/YOGIITSU/dto/InquiryResponseDto.java
@@ -0,0 +1,25 @@
+package com.YOGIITSU.dto;
+
+import com.YOGIITSU.entity.Inquiry;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class InquiryResponseDto {
+    // 문의 정보를 클라이언트에 반환할 때 사용하는 dto
+    private Long inquiryId;
+    private String inquiryContent;
+    private String response;
+    private LocalDateTime inquiryAt;
+    private LocalDateTime responseAt;
+
+    public InquiryResponseDto(Inquiry inquiry) {
+        // Inquiry 엔티티 기반으로 필요한 필드만 dto로 변환
+        this.inquiryId = inquiry.getInquiryId();
+        this.inquiryContent = inquiry.getInquiryContent();
+        this.response = inquiry.getResponse();
+        this.inquiryAt = inquiry.getInquiryAt();
+        this.responseAt = inquiry.getResponseAt();
+    }
+}
