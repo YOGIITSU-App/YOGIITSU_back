ccbeab51dea9e2cd5a236f5dad506de0825ffca6
parksomii <somi020605@gmail.com>
2025-01-31 00:26:13 +0900

    feat: MemberRepository에 이메일로 회원을 찾는 메서드 추가

--- src/main/java/com/YOGIITSU/repository/MemberRepository.java
+++ src/main/java/com/YOGIITSU/repository/MemberRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Optional<Member> findByMemberId(String memberId);
+	// 이메일을 기준으로 회원을 찾는 메서드 추가
+	Optional<Member> findByEmail(String email);
 }
