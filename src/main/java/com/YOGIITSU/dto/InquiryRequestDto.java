f1e39f5675d257e97bab0a438a11344f8abf59c8
joyes0ng <adelinays1020@naver.com>
2025-02-02 16:56:00 +0900

    refactor: InquiryRequestDto에 @AllArgsConstructor 추가

--- src/main/java/com/YOGIITSU/dto/InquiryRequestDto.java
+++ src/main/java/com/YOGIITSU/dto/InquiryRequestDto.java
@@ -1,10 +1,12 @@
 package com.YOGIITSU.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class InquiryRequestDto {
     // 문의 내용 작성 시 필요한 데이터 받기 위함
     private String inquiryContent;  // 문의 내용
