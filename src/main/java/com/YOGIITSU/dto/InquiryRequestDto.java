f2c02411c0eed00bbc31ac05c5b7636293f6cbe3
joyes0ng <adelinays1020@naver.com>
2025-02-03 17:13:53 +0900

    refactor: InquiryRequestDto의 Setter 제거

--- src/main/java/com/YOGIITSU/dto/InquiryRequestDto.java
+++ src/main/java/com/YOGIITSU/dto/InquiryRequestDto.java
@@ -2,10 +2,8 @@ package com.YOGIITSU.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class InquiryRequestDto {
     // 문의 내용 작성 시 필요한 데이터 받기 위함
