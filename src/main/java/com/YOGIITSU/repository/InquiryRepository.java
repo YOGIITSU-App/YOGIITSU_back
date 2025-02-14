4e97292ad158ae27029e7756ec78a9ad29fd38a3
joyes0ng <adelinays1020@naver.com>
2025-02-15 03:11:13 +0900

    refactor: InquiryRepository에 findByInquiryId 메서드 추가

--- src/main/java/com/YOGIITSU/repository/InquiryRepository.java
+++ src/main/java/com/YOGIITSU/repository/InquiryRepository.java
@@ -4,7 +4,9 @@ import com.YOGIITSU.entity.Inquiry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
     List<Inquiry> findByMemberId(Long memberId);  // 특정 회원 문의 조회
+    Optional<Inquiry> findByInquiryId(Long inquiryId);  // 특정 문의 1개 조회
 }
