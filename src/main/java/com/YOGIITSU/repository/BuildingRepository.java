5f8536b3231d294e4bf6b32e6e329f06275b39b2
parksomii <somi020605@gmail.com>
2025-02-23 01:01:51 +0900

    feat: BuildingRepository 인터페이스 추가

---
+++ src/main/java/com/YOGIITSU/repository/BuildingRepository.java
@@ -0,0 +1,8 @@
+package com.YOGIITSU.repository;
+
+import com.YOGIITSU.entity.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuildingRepository extends JpaRepository<Building, Long> {
+
+}
