6a8042ccbfeb31211c81166eaeb3a0979636f107
parksomii <somi020605@gmail.com>
2025-03-19 14:21:32 +0900

    fix: DTO 직렬화 오류 해결을 위한 어노테이션 추가
    - Spring Boot 3.3.1에서 JSON 직렬화 방식이 변경되어 기본 생성자가 필요해짐

--- src/main/java/com/YOGIITSU/dto/RequestDto/SearchKeywordRequestDto.java
+++ src/main/java/com/YOGIITSU/dto/RequestDto/SearchKeywordRequestDto.java
@@ -2,9 +2,11 @@ package com.YOGIITSU.dto.RequestDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 
 public class SearchKeywordRequestDto {
 
