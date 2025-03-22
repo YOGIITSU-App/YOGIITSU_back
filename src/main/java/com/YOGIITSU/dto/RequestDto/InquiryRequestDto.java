fa25de869dc9f4d555df3a62e3f7f89622f5d88f
joyes0ng <adelinays1020@naver.com>
2025-03-22 16:57:38 +0900

    fix: 충돌 해결 및 InquiryRequestDto 정리

--- src/main/java/com/YOGIITSU/dto/RequestDto/InquiryRequestDto.java
+++ src/main/java/com/YOGIITSU/dto/RequestDto/InquiryRequestDto.java
@@ -9,5 +9,5 @@ public class InquiryRequestDto {
     // 문의 내용 작성 시 필요한 데이터 받기 위함
     private String inquiryTitle;  // 문의 제목
     private String inquiryContent;  // 문의 내용
-    private String passwordHash;  // 비밀번호 검증용
+    private String password;  // 사용자 검증용 비밀번호
 }
