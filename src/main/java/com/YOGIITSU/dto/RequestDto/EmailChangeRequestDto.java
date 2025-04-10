32b6216579950b5329b2fcc78525ead74daa0663
gayoung228 <103436798+gayoung228@users.noreply.github.com>
2025-04-10 20:37:14 +0900

    feat: EmailChangeRequestDto에 이메일 변경 요청 DTO 추가

--- src/main/java/com/YOGIITSU/dto/RequestDto/EmailChangeRequestDto.java
+++ src/main/java/com/YOGIITSU/dto/RequestDto/EmailChangeRequestDto.java
@@ -1,5 +1,18 @@
 package com.YOGIITSU.dto.RequestDto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmailChangeRequestDto {
 
+    @NotBlank(message = "새 이메일은 필수입니다.")
+    @Email(message = "올바른 이메일 형식이어야 합니다.")
+    private String newEmail;
 }
+
