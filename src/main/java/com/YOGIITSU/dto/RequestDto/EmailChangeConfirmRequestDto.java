499c013c19b29bebabae85df0acdd294250a35a7
gayoung228 <103436798+gayoung228@users.noreply.github.com>
2025-04-10 20:36:16 +0900

    feat: EmailChangeConfirmRequestDto에 이메일 변경 확정 요청 DTO 추가

--- src/main/java/com/YOGIITSU/dto/RequestDto/EmailChangeConfirmRequestDto.java
+++ src/main/java/com/YOGIITSU/dto/RequestDto/EmailChangeConfirmRequestDto.java
@@ -1,5 +1,16 @@
 package com.YOGIITSU.dto.RequestDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmailChangeConfirmRequestDto {
 
+    private String newEmail;
+    private String token;
+    private String code;
 }
+
