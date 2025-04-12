5fdc222e3629b82f965e3d8bf77bc289279f47b3
joyes0ng <adelinays1020@naver.com>
2025-04-12 12:41:36 +0900

    refactor: InquiryRequestDto에 문의 전용 비밀번호 필드 및 유효성 검증 추가

--- src/main/java/com/YOGIITSU/dto/RequestDto/InquiryRequestDto.java
+++ src/main/java/com/YOGIITSU/dto/RequestDto/InquiryRequestDto.java
@@ -1,5 +1,6 @@
 package com.YOGIITSU.dto.RequestDto;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,5 +10,7 @@ public class InquiryRequestDto {
     // 문의 내용 작성 시 필요한 데이터 받기 위함
     private String inquiryTitle;  // 문의 제목
     private String inquiryContent;  // 문의 내용
-    private String password;  // 사용자 검증용 비밀번호
+
+    @Size(min = 4, max = 20, message = "비밀번호는 4 ~ 20자의 영문 또는 숫자만 입력하세요.")
+    private String inquiryPassword;  // 사용자 검증용 비밀번호
 }
