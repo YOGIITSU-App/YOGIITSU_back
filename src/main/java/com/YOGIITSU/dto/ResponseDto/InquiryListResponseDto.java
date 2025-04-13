305d9e63e75cd71c6c0e2842561d1f6f5aabb324
joyes0ng <adelinays1020@naver.com>
2025-04-13 20:21:43 +0900

    feat: InquiryListResponseDto에 inquiryAt 필드 추가

--- src/main/java/com/YOGIITSU/dto/ResponseDto/InquiryListResponseDto.java
+++ src/main/java/com/YOGIITSU/dto/ResponseDto/InquiryListResponseDto.java
@@ -1,6 +1,7 @@
 package com.YOGIITSU.dto.ResponseDto;
 
 import com.YOGIITSU.entity.Inquiry;
+import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
@@ -8,10 +9,12 @@ public class InquiryListResponseDto {
     private final String inquiryTitle;
     private final String authorName;
     private final String inquiryState;
+    private final LocalDateTime inquiryAt;
 
     public InquiryListResponseDto(Inquiry inquiry) {
         this.inquiryTitle = inquiry.getInquiryTitle();
         this.authorName = inquiry.getMember().getUsername();
         this.inquiryState = inquiry.getInquiryState().name();  // "PROCESSING", "COMPLETED"
+        this.inquiryAt = inquiry.getInquiryAt();
     }
 }
