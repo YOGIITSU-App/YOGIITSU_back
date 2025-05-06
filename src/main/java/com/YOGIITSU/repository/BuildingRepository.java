ebddbab5d66947aacab68b65ce30d6ab08fc0721
parksomii <somi020605@gmail.com>
2025-05-07 02:31:48 +0900

    feat: Building 상세 조회용 연관관계 포함 쿼리 추가

--- src/main/java/com/YOGIITSU/repository/BuildingRepository.java
+++ src/main/java/com/YOGIITSU/repository/BuildingRepository.java
@@ -2,11 +2,22 @@ package com.YOGIITSU.repository;
 
 import com.YOGIITSU.entity.Building;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BuildingRepository extends JpaRepository<Building, Long> {
 
 	// 입력된 검색어(query)가 포함된 건물 이름을 최대 6개까지 반환
 	List<Building> findTop6ByNameContainingOrderByNameAsc(String query);
 
+	// 건물 ID로 건물 조회
+	@Query("SELECT b FROM Building b " +
+		"LEFT JOIN FETCH b.buildingTags " +
+		"LEFT JOIN FETCH b.buildingFacilities " +
+		"LEFT JOIN FETCH b.buildingFloorImages " +
+		"LEFT JOIN FETCH b.departments " +
+		"WHERE b.id = :id")
+	Optional<Building> findByIdWithAllRelations(@Param("id") Long id);
 }
