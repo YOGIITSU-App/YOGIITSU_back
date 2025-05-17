f7245da77d5698e96d39c448e724f4f8a83e8961
gayoung228 <103436798+gayoung228@users.noreply.github.com>
2025-05-17 17:32:17 +0900

    refactor: EmailMessage에 목적(enum) 필드 추가

--- src/main/java/com/YOGIITSU/entity/EmailMessage.java
+++ src/main/java/com/YOGIITSU/entity/EmailMessage.java
@@ -2,7 +2,6 @@ package com.YOGIITSU.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.LocalDateTime;
 
 @Entity
@@ -13,33 +12,36 @@ import java.time.LocalDateTime;
 @Builder
 public class EmailMessage {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "email_message_id")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "email_message_id")
+    private Long id;
 
-	@Column(nullable = false, length = 255)
-	private String email;
+    @Column(nullable = false, length = 255)
+    private String email;
 
-	@Column(nullable = false, length = 255)
-	private String code;
+    @Column(nullable = false, length = 255)
+    private String code;
 
-	@Column(name = "is_approved", nullable = false, columnDefinition = "TINYINT(1)")
-	private Boolean isApproved;
+    @Column(name = "is_approved", nullable = false, columnDefinition = "TINYINT(1)")
+    private Boolean isApproved;
 
-	@Column(name = "expires_at", nullable = false)
-	private LocalDateTime expiresAt;
+    @Column(name = "expires_at", nullable = false)
+    private LocalDateTime expiresAt;
 
-	// 인증 코드 갱신 메서드
-	public void updateCode(String code) {
-		this.code = code;
-		this.isApproved = false;
-		this.expiresAt = LocalDateTime.now().plusMinutes(5); // 5분 후 만료 시간 갱신
-	}
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EmailPurpose purpose;
 
-	// 인증 성공 시 승인 처리
-	public void setIsApproved(Boolean isApproved) {
-		this.isApproved = isApproved;
-	}
+    // 인증 코드 갱신 메서드
+    public void updateCode(String code) {
+        this.code = code;
+        this.isApproved = false;
+        this.expiresAt = LocalDateTime.now().plusMinutes(5); // 5분 후 만료 시간 갱신
+    }
 
+    // 인증 성공 시 승인 처리
+    public void setIsApproved(Boolean isApproved) {
+        this.isApproved = isApproved;
+    }
 }
