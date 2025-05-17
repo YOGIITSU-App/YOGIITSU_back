d94b99ad21ac004346ecb545b6ae509bc53166f7
gayoung228 <103436798+gayoung228@users.noreply.github.com>
2025-05-17 17:33:33 +0900

    refactor: EmailPurpose에 이메일 인증 목적 분리를 위한 EmailPurpose enum 정의

--- src/main/java/com/YOGIITSU/entity/EmailPurpose.java
+++ src/main/java/com/YOGIITSU/entity/EmailPurpose.java
@@ -1,5 +1,8 @@
 package com.YOGIITSU.entity;
 
-public class EmailPurpose {
-
+// 이메일 변경 목적
+public enum EmailPurpose {
+    SIGNUP, // 회원가입 이메일 전송
+    NORMAL, // 이메일 전송해야 할 때(예를 들어, 이메일 변경 전 기존 이메일 전송할 때 등..)
+    EMAIL_CHANGE // 이메일 변경할 때 이메일 전송
 }
