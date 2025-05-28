2c390951038f5ea171045631723564930abde089
gayoung228 <gayoung030228@gmail.com>
2025-05-28 17:57:38 +0900

    refactor: EmailPostRequestDto에 purpose 필드 제거

--- src/main/java/com/YOGIITSU/dto/RequestDto/EmailPostRequestDto.java
+++ src/main/java/com/YOGIITSU/dto/RequestDto/EmailPostRequestDto.java
@@ -1,6 +1,5 @@
 package com.YOGIITSU.dto.RequestDto;
 
-import com.YOGIITSU.entity.EmailPurpose;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -12,8 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class EmailPostRequestDto {
 
-	@NotBlank(message = "이메일은 필수 입력 값입니다.")
-	@Email(message = "올바른 이메일 형식이어야 합니다.")
-	private String email;
-	private EmailPurpose purpose;
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "올바른 이메일 형식이어야 합니다.")
+    private String email;
 }
