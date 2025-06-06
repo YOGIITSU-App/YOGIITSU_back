7c9aedfcc255cfa593a7d5cfaf84c8cc0e0c80a0
parksomii <somi020605@gmail.com>
2025-06-06 22:30:59 +0900

    feat: FacilityMarkerResponseDto 생성 (이름, 위도, 경도 반환용)

---
+++ src/main/java/com/YOGIITSU/dto/ResponseDto/FacilityMarkerResponseDto.java
@@ -0,0 +1,13 @@
+package com.YOGIITSU.dto.ResponseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FacilityMarkerResponseDto {
+
+	private String name;
+	private Double latitude;
+	private Double longitude;
+}
