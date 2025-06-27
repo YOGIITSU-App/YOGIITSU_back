c2933fa4fa3f66832557260e333e9cfcb899420a
parksomii <somi020605@gmail.com>
2025-06-27 18:20:37 +0900

    fix: building 연관관계 EAGER → LAZY, nullable false → true로 변경

--- src/main/java/com/YOGIITSU/entity/RecentSearch.java
+++ src/main/java/com/YOGIITSU/entity/RecentSearch.java
@@ -27,7 +27,7 @@ public class RecentSearch {
 	@Column(name = "searched_at", nullable = false)
 	private LocalDateTime searchedAt;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "building_id", nullable = false)
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "building_id", nullable = true)
 	private Building building;
 }
