1c9bceea9bdb5f250067278ecd52f5f8788d3442
gayoung228 <gayoung030228@gmail.com>
2025-06-29 17:33:11 +0900

    feat: NoticeRepository에 공지사항 최신순 조회 메서드 구현

---
+++ src/main/java/com/YOGIITSU/repository/NoticeRepository.java
@@ -0,0 +1,9 @@
+package com.YOGIITSU.repository;
+
+import com.YOGIITSU.entity.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    List<Notice> findAllByOrderByNoticeAtDesc();
+}
