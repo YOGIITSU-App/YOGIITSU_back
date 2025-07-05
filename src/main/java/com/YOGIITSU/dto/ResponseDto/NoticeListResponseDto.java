1483fcefff53fdcbd29a2ca208095ae3967472e9
gayoung228 <gayoung030228@gmail.com>
2025-07-05 19:09:01 +0900

    feat: NoticeListResponseDto 클래스 추가 (공지사항 전체 조

--- src/main/java/com/YOGIITSU/dto/ResponseDto/NoticeListResponseDto.java
+++ src/main/java/com/YOGIITSU/dto/ResponseDto/NoticeListResponseDto.java
@@ -1,4 +1,14 @@
 package com.YOGIITSU.dto.ResponseDto;
 
-public class NoticeListResponseDto {
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NoticeListResponseDto { // 공지사항 전체 조회
+
+    private Long noticeId;                // 공지사항 ID
+    private String noticeTitle;           // 공지사항 제목
+    private LocalDateTime noticeAt; // 작성일시
 }
