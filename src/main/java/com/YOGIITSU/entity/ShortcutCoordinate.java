e59766fb5b34516175f448f1af7e5328189f3cac
joyes0ng <adelinays1020@naver.com>
2025-07-07 02:24:18 +0900

    feat: 지름길 좌표 저장을 위한 ShortcutCoordinate 엔티티 생성

---
+++ src/main/java/com/YOGIITSU/entity/ShortcutCoordinate.java
@@ -0,0 +1,46 @@
+package com.YOGIITSU.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "shortcut_coordinate")
+public class ShortcutCoordinate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "coordinate_id", updatable = false, nullable = false)
+    private Long coordinateId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shortcut_id", nullable = false)
+    private Shortcut shortcut;
+
+    @Column(name = "latitude", nullable = false)
+    private Double latitude;
+
+    @Column(name = "longitude", nullable = false)
+    private Double longitude;
+
+    @Column(name = "point_order", nullable = false)
+    private Integer pointOrder;
+
+    public void setShortcut(Shortcut shortcut) {
+        this.shortcut = shortcut;
+    }
+}
