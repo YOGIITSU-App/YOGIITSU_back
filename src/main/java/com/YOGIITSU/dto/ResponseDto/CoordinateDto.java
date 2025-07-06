b1169ba9d403561833af96570d7c1304af427f98
joyes0ng <adelinays1020@naver.com>
2025-07-07 02:45:41 +0900

    feat: 위도/경도 응답 CoordinateDto 추가

---
+++ src/main/java/com/YOGIITSU/dto/ResponseDto/CoordinateDto.java
@@ -0,0 +1,13 @@
+package com.YOGIITSU.dto.ResponseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CoordinateDto {
+
+    private Double latitude;
+    private Double longitude;
+
+}
