91d2f6b102ac68ea121bb67241b17bce82ab691f
joyes0ng <adelinays1020@naver.com>
2025-07-07 22:30:35 +0900

    refactor: Shortcut 엔티티에서 description 필드 제거

--- src/main/java/com/YOGIITSU/entity/Shortcut.java
+++ src/main/java/com/YOGIITSU/entity/Shortcut.java
@@ -28,23 +28,14 @@ public class Shortcut {
     @Column(name = "shortcut_id", updatable = false, nullable = false)
     private Long shortcutId;
 
-    @Column(name = "shortcut_name", nullable = false)
-    private String shortcutName;
+    @Column(name = "point_a", nullable = false)
+    private String pointA;
 
-    @Column(name = "start_point", nullable = false)
-    private String startPoint;
-
-    @Column(name = "end_point", nullable = false)
-    private String endPoint;
-
-    @Column(name = "description", columnDefinition = "TEXT")
-    private String description;
+    @Column(name = "point_b", nullable = false)
+    private String pointB;
 
     @Column(name = "distance", nullable = false)
     private Double distance;
-
-    @Column(name = "image_url")
-    private String imageUrl;
 
     @OneToMany(mappedBy = "shortcut", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ShortcutCoordinate> coordinates = new ArrayList<>();
