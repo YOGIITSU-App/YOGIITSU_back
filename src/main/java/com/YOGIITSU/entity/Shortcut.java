ac98f6c38e2996651cb85ae275197d3daa82c469
joyes0ng <adelinays1020@naver.com>
2025-07-10 18:45:18 +0900

    feat: 지름길 정보를 위한 Shortcut 엔티티 생성

---
+++ src/main/java/com/YOGIITSU/entity/Shortcut.java
@@ -0,0 +1,46 @@
+package com.YOGIITSU.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Table(name = "shortcut")
+public class Shortcut {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "shortcut_id", updatable = false, nullable = false)
+    private Long shortcutId;
+
+    @Column(name = "point_a", nullable = false)
+    private String pointA;
+
+    @Column(name = "point_b", nullable = false)
+    private String pointB;
+
+    @Column(name = "distance", nullable = false)
+    private Double distance;
+
+    @Column(name = "duration", nullable = false)
+    private Integer duration;
+
+    @OneToMany(mappedBy = "shortcut", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ShortcutCoordinate> coordinates = new ArrayList<>();
+}
