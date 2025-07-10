906a59a2082df01a91c231d7140b592adb86f8ce
joyes0ng <adelinays1020@naver.com>
2025-07-10 18:47:21 +0900

    feat: 지름길 상세 조회용 ShortcutDetailResponseDto 추가

---
+++ src/main/java/com/YOGIITSU/dto/ResponseDto/ShortcutDetailResponseDto.java
@@ -0,0 +1,18 @@
+package com.YOGIITSU.dto.ResponseDto;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ShortcutDetailResponseDto {
+
+    private Long shortcutId;
+    private String pointA;
+    private String pointB;
+    private Double distance;
+    private Integer duration;
+    private List<CoordinateDto> coordinates;
+
+}
