03a043daec726ae0964288554ff88206bfa67ca0
parksomii <somi020605@gmail.com>
2025-07-10 14:45:47 +0900

    feat: 스웨거 문서에 개발 서버 주소 추가

--- src/main/java/com/YOGIITSU/config/SwaggerConfig.java
+++ src/main/java/com/YOGIITSU/config/SwaggerConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(
 	servers = {
 		@Server(url = "https://yogiitsu.com", description = "배포 서버"),
+		@Server(url = "https://dev.yogiitsu.com", description = "개발 서버"),
 		@Server(url = "http://localhost:8080", description = "로컬 서버")
 	}
 )
