689006a3982efab386e3a5148fafb92585f9888e
joyes0ng <adelinays1020@naver.com>
2025-09-25 13:31:47 +0900

    feat: 마이페이지 프로필 구현을 위한 MypageProfileResponseDto 추가

---
+++ src/main/java/com/YOGIITSU/dto/ResponseDto/MypageProfileResponseDto.java
@@ -0,0 +1,16 @@
+package com.YOGIITSU.dto.ResponseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class MypageProfileResponseDto {
+
+    private String userName;  // 사용자 이름
+    private String email;  // 이메일
+    private String memberId;  // 로그인 ID
+    private String provider;  // 가입 경로 (local, google, kakao, apple 등)
+}
