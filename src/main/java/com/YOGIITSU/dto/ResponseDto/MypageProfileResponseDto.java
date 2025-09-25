65bbd80b210c50157902f5e5b7426d56e592cb90
joyes0ng <adelinays1020@naver.com>
2025-09-25 14:03:39 +0900

    refactor: MypageProfileResponseDto에서 DTO 필드 순서 변경

--- src/main/java/com/YOGIITSU/dto/ResponseDto/MypageProfileResponseDto.java
+++ src/main/java/com/YOGIITSU/dto/ResponseDto/MypageProfileResponseDto.java
@@ -9,7 +9,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class MypageProfileResponseDto {
 
+    private String memberId;  // 로그인 ID
     private String userName;  // 사용자 이름
     private String email;  // 이메일
-    private String memberId;  // 로그인 ID
 }
