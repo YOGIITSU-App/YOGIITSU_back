381f49dd7d7d98526036b4e1afa832cfbd29ed09
parksomii <somi020605@gmail.com>
2025-09-29 15:58:05 +0900

    refactor: 기존 예외 클래스 ErrorCode 기반으로 리팩토링
    - AppleExchangeException을 ExternalServiceException으로 변경
    - BuildingNotFoundException을 ResourceException으로 변경
    - CafeteriaNotFoundForBuildingException을 ResourceException으로 변경
    - 모든 예외가 BaseException을 상속하고 ErrorCode를 사용하도록 통일

--- src/main/java/com/YOGIITSU/exception/building/BuildingNotFoundException.java
+++ src/main/java/com/YOGIITSU/exception/building/BuildingNotFoundException.java
@@ -1,8 +1,18 @@
 package com.YOGIITSU.exception.building;
 
-public class BuildingNotFoundException extends RuntimeException {
+import com.YOGIITSU.exception.resource.ResourceException;
+import com.YOGIITSU.exception.ErrorCode;
+
+/**
+ * 건물을 찾을 수 없는 예외
+ */
+public class BuildingNotFoundException extends ResourceException {
 
 	public BuildingNotFoundException(Long buildingId) {
-		super("존재하지 않는 건물입니다. buildingId=" + buildingId);
+		super(ErrorCode.BUILDING_NOT_FOUND, "buildingId=" + buildingId);
+	}
+
+	public BuildingNotFoundException() {
+		super(ErrorCode.BUILDING_NOT_FOUND);
 	}
 }
