2554fac1007a2231db157359f0dd95e7dd64777f
joyes0ng <adelinays1020@naver.com>
2025-10-06 16:15:59 +0900

    refactor: 마이페이지 프로필 DTO 및 예외처리 로직 개선

--- src/main/java/com/YOGIITSU/dto/ResponseDto/MypageProfileResponseDto.java
+++ src/main/java/com/YOGIITSU/dto/ResponseDto/MypageProfileResponseDto.java
@@ -2,9 +2,9 @@ package com.YOGIITSU.dto.ResponseDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @Builder
 @AllArgsConstructor
 public class MypageProfileResponseDto {
