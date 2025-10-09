4a2700d2963e4a9bbaf02060dc1b217bc13e4928
joyes0ng <adelinays1020@naver.com>
2025-10-10 01:46:11 +0900

    fix: Apple 로그인 응답에서 userId 제거

--- src/main/java/com/YOGIITSU/controller/AppleAuthController.java
+++ src/main/java/com/YOGIITSU/controller/AppleAuthController.java
@@ -50,7 +50,6 @@ public class AppleAuthController {
         UserResponseDto userDto = tokenInfo.getUser();
         Map<String, Object> responseBody = new HashMap<>();
         responseBody.put("message", "로그인 성공");
-        responseBody.put("userId", userDto.getId());
         responseBody.put("role", userDto.getRole());
 
         log.debug("[AppleAuthController] Apple 로그인 성공 - userId={}, role={}",
